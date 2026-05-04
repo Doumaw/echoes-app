b183ee73d47f565516f094203cb544e1205ac1ea
Skrinja <simonclement94@gmail.com>
2026-05-04 22:31:32 +0200

    Update aiService.ts

--- services/aiService.ts
+++ services/aiService.ts
@@ -11,7 +11,7 @@ export const aiService = {
     if (!API_KEY) throw new Error("API Key manquante dans le .env");
 
     const formattedHistory = [...history].reverse().map(msg => ({
-      role: msg.isUser ? 'user' : 'assistant',
+      role: msg.isUser ? 'user' : 'ia',
       content: msg.text
     }));
 
